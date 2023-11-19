da2201351298814af8813b0291f5fe337985583d
Brandon Tan <brandontan2003@gmail.com>
2023-11-19 19:13:19 +0800

    Add Update SQL Query

--- main/constant/GenerateTransactionIdConstant.py
+++ main/constant/GenerateTransactionIdConstant.py
@@ -2,6 +2,6 @@ select_id_from_generate_transaction_id = "SELECT id FROM generate_transaction_id
 
 update_new_transaction_id = "UPDATE generate_transaction_id SET id = %s WHERE created_date = CURRENT_DATE;"
 
-query_date_from_transaction_id = "SELECT created_date FROM generate_transaction_id ORDER BY id DESC LIMIT 1;"
+query_date_from_transaction_id = "SELECT created_date FROM generate_transaction_id ORDER BY created_date DESC LIMIT 1;"
 
 insert_default_value_transaction_id = "INSERT INTO generate_transaction_id (id, created_date) VALUES (0, CURRENT_DATE);"
