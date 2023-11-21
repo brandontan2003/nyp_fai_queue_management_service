b2341a7a7af53fe69411e4a10060afbabb9638ac
Brandon Tan <brandontan2003@gmail.com>
2023-11-21 08:16:19 +0800

    Add cancelling appointment in database after successful application

--- main/data/entity/QueueTransactionDataAccess.py
+++ main/data/entity/QueueTransactionDataAccess.py
@@ -20,7 +20,7 @@ class QueueTransactionDataAccess:
         if result:
             return QueueTransaction(transaction_id=result[0], symptoms=result[1], status=result[2])
         else:
-            return ResponsePayload(transaction_id_not_found)
+            return ResponsePayload(transaction_id_not_found).to_dict()
 
     def insert_queue_transaction_record(self, transaction_id, symptoms, status):
         self.cursor.execute(insert_into_queue_transaction_sql, (transaction_id, symptoms, status))
@@ -34,13 +34,17 @@ class QueueTransactionDataAccess:
         if result:
             if queue.status == in_queue and status == cancelled:
                 self.cursor.execute(update_queue_transaction_sql % (status, transaction_id))
-                return ResponsePayload(transaction_cancelled_successfully % transaction_id)
+                return ResponsePayload(transaction_cancelled_successfully % transaction_id).to_dict()
+
             elif queue.status == in_queue and status == registered:
                 self.cursor.execute(update_queue_transaction_sql % (status, transaction_id))
-                return ResponsePayload(transaction_registered_successfully % transaction_id)
+                return ResponsePayload(transaction_registered_successfully % transaction_id).to_dict()
+
             elif queue.status == cancelled:
-                ResponsePayload(transaction_status_already_cancelled)
+                ResponsePayload(transaction_status_already_cancelled).to_dict()
+
             elif queue.status == registered:
-                ResponsePayload(transaction_status_already_registered)
+                ResponsePayload(transaction_status_already_registered).to_dict()
+
         else:
-            return ResponsePayload(transaction_id_not_found)
+            return ResponsePayload(transaction_id_not_found).to_dict()
