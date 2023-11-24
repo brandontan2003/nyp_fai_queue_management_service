772d3b497a0d3d6780f479556eda1f2db6f7286e
Brandon Tan <brandontan2003@gmail.com>
2023-11-24 23:25:23 +0800

    Add prod_db variables

--- main/config/QueueManagementDBConfig.py
+++ main/config/QueueManagementDBConfig.py
@@ -1,11 +1,13 @@
 import mysql.connector
 import os
-env = os.environ.get('ENVIRONMENT', 'local')  # Default to development if ENVIRONMENT is not set
+env = os.environ.get('ENVIRONMENT', 'production')  # Default to development if ENVIRONMENT is not set
 
 if env == 'development':
     from main.config.config_dev import DB_CONFIG
 elif env == 'local':
     from main.config.config_local import DB_CONFIG
+elif env == 'production':
+    from main.config.config_prod import DB_CONFIG
 else:
     raise ValueError(f"Invalid environment: {env}")
 
---
+++ main/config/config_prod.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'bxkwchimme1ebrqsss8o-mysql.services.clever-cloud.com',
+    'port': '3306',
+    'user': 'uag4gifkex3870yw',
+    'password': 'DQqV4rQHAGhjPpaweDIo',
+    'database': 'bxkwchimme1ebrqsss8o',
+}
